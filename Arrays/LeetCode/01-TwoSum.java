ee5e75b14a6763322b4926890a3f29e9c8266e8f
MD ARMAN ALAM <armanalamon@gmail.com>
2026-07-15 10:15:26 +0530

    Add twoSum method in Solution class
    Implement twoSum method to find indices of two numbers that add up to a target.

---
+++ Arrays/LeetCode/01-TwoSum.java
@@ -0,0 +1,24 @@
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+       
+        for(int i = 0; i<nums.length; i++)
+        {
+            int next = target - nums[i];
+            if(hm.containsKey(next))
+            {
+                return new int[] {i , hm.get(next)};
+            }
+            hm.put(nums[i], i);
+        }
+       
+            return new int[0];
+           
+
+
+        }
+}
+        
+    
