456c3f75309a7cde77a8e82129972a9cc3a81956
MD ARMAN ALAM <armanalamon@gmail.com>
2026-09-20 07:49:15 +0530

    Add reorderList method for linked list reordering
    Implement reorderList method to reorder a linked list in-place.

---
+++ LinkedList/lc_143ReorderList.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+          if(head==null || head.next==null){
+            return;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+      
+
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid=slow;
+        ListNode preCurr=slow.next;
+        while(preCurr.next!=null){
+            ListNode curr=preCurr.next;
+            preCurr.next=curr.next;
+            curr.next=mid.next;
+            mid.next=curr;
+        }
+        slow=head;
+        fast=mid.next;
+        while(slow!=mid){
+            mid.next=fast.next;
+            fast.next=slow.next;
+            slow.next=fast;
+            slow=fast.next;
+            fast=mid.next;
+        }
+
+    }
+}
