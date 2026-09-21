7775aae0ac290b6130db5432ab0d1fe659aa889c
MD ARMAN ALAM <armanalamon@gmail.com>
2026-09-22 05:07:08 +0530

    Implement rotateRight method for linked list

---
+++ LinkedList/med/lc_61RotateList.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null|| k==0  ){
+            return head;
+        }
+        int size=1;
+        ListNode last=head;
+     while(last.next!=null) 
+     {
+        last=last.next;
+        size++;
+     }  
+
+     k=k%size;
+     if(k==0){
+        return head;
+     }
+
+     last.next=head;
+     int tailidx=size-k;
+     ListNode newTail=head;
+     for(int i=1; i<tailidx; i++)
+    {
+        newTail=newTail.next;
+    }
+     ListNode newHead=newTail.next;
+     newTail.next=null;
+
+
+     return newHead;
+
+
+     
+
+  
+
+    }
+}
